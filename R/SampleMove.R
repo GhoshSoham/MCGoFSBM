8ac45374fcae9217635d7e8a1bc286efddff39b6
Soham Ghosh <sohamghosh.isical@gmail.com>
2022-11-19 17:30:57 -0600

    change sample function to sample.init

--- R/SampleMove.R
+++ R/SampleMove.R
@@ -3,8 +3,8 @@
 
 sample_a_move <- function(C, G_current) {
   # Input:
-  # G_current: igraph object and undirected graph
-  # C: vector of block assignment from 1 to k
+  # G_current: igraph object which is an undirected graph and has no self loop
+  # C: numeric vector of size n of block assignment; from 1 to k
 
   # Getting block information
   num_blocks <- length(unique(C))
@@ -18,12 +18,12 @@ sample_a_move <- function(C, G_current) {
   comp_edges <- igraph::get.edgelist(G_comp)
 
   # Determine whether the move is inter-block or intra-block
-  type <- sample(2, size = 1)
+  type <- sample.int(2, size = 1)
 
   if (type == 1) {
     # Adding and deleting edges from same block
     # Sample a block
-    s <- sample(num_blocks, size = 1)
+    s <- sample.int(num_blocks, size = 1)
 
     # Find edges within the sampled block for both the graph and complement graph
     to_delete <- all_edges[((C[as.numeric(all_edges[, 1])] == s) * (C[as.numeric(all_edges[, 2])] == s)) > 0, ]
@@ -32,8 +32,8 @@ sample_a_move <- function(C, G_current) {
     # Check whether the sampled block has at least one edge
     if ((length(to_delete) > 0) * (length(to_add) > 0)) {
       # Sample an edge to add from complement graph and delete from the graph
-      delete_edge <- sample(nrow(to_delete), 1)
-      add_edge <- sample(nrow(to_add), 1)
+      delete_edge <- sample.int(nrow(to_delete), 1)
+      add_edge <- sample.int(nrow(to_add), 1)
 
       # Add and delete an edge in the same block
       G_sample <- igraph::add_edges(G_current, to_add[add_edge, ])
@@ -44,7 +44,7 @@ sample_a_move <- function(C, G_current) {
   } else if (type == 2) {
     # Adding and deleting edges between two different blocks
     # Sample two different blocks
-    two_blocks <- sample(num_blocks, 2, replace = FALSE)
+    two_blocks <- sample.int(num_blocks, size = 2, replace = FALSE)
     s <- two_blocks[1]
     t <- two_blocks[2]
 
@@ -55,8 +55,8 @@ sample_a_move <- function(C, G_current) {
     # Check whether the sampled blocks have at least one edge
     if ((length(inter) > 0) * (length(comp_inter) > 0)) {
       # Sample an edge to add from complement graph and delete from the graph
-      delete_edge <- sample(nrow(inter), 1)
-      add_edge <- sample(nrow(comp_inter), 1)
+      delete_edge <- sample.int(nrow(inter), 1)
+      add_edge <- sample.int(nrow(comp_inter), 1)
 
       # Add and delete an edge between two fixed block
       G_sample <- igraph::add_edges(G_current, comp_inter[add_edge, ])
