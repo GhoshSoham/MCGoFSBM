ff09bb096d427bedcd3ecc44433964c2e8ed3ee7
Soham Ghosh <sohamghosh.isical@gmail.com>
2022-11-22 20:12:01 -0600

    change the input from igraph object to an adjacency matrix and change compatibility check accordingly, and output to statistic value of the observed graph and p-value

--- R/GoFTest.R
+++ R/GoFTest.R
@@ -1,38 +1,45 @@
 # The true model is ERSBM
-# Chi-Square goodness of fit test statistic computation using the observed graph and MLE
+# Testing whether the observed graph follows ERSBM model using chi square goodness of fit test
+# for network data
 
 #' Title
+#' @title Monte Carlo Goodness of Fit tests for Stochastic Block Models
 #'
-#' @param G_obs igraph object which is an undirected graph and has no self loop
+#' @param A a n by n binary symmetric adjacency matrix representing a undirected graph where n is the no nodes in the graph
 #' @param C numeric vector of size n of block assignment; from 1 to k
 #' @param numGraphs number of graphs will be sampled; default value is 100
 #'
-#' @return chi_seq: sequence of chi square test statistics on the sampled graphs
-#' @return pvalue: estimated p-value when true model is ERSBM
+#' @return A list with the elements
+#' \item{statistic}{Value of the chi-square test statistic}
+#' \item{p.value}{the p-value for the test.}
+#'
 #' @export
 #'
 #' @examples
-goftest <- function(G_obs, C, numGraphs = 100) {
-  if (!inherits(G_obs, "igraph")) {
-    stop("The input G_obs should be an igraph object")
+goftest <- function(A, C, numGraphs = 100) {
+  # Some compatibility checks and error message
+  if (!is.matrix(A)) {
+    stop("A should be an adjacency matrix of the graph.")
+  }
+  if (!isSymmetric.matrix(A)) {
+    stop("A should be a square symmetric matrix.")
+  }
+  if (!all(A %in% c(0,1) )) {
+    stop("A can only contain 0's and 1's.")
+  }
+  if (!all(diag(A) == 0)) {
+    stop("All the diagonal entries of A should be 0")
   }
 
-  if (igraph::is.weighted(G_obs)) {
-    stop("The input graph G_obs shouldn't be a weighted one")
-  }
 
-  if (igraph::is_directed(G_obs)) {
-    stop("The input graph G_obs shouldn't be a directed one")
-  }
-
-  if (igraph::any_loop(G_obs)) {
-    stop("The graph G_obs shouldn't have any self loop")
-  }
-
-  n <- igraph::gorder(G_obs)
+  # Getting dimension information
+  n <- nrow(A)
   if (length(C) != n) {
-    stop("The C should have same length as no of vertices of graph")
+    stop("The C should have same length as no of rows in A")
   }
+
+  # Getting an igraph (an undirected and unweighted) object from the input adjacency matrix
+  G_obs <- igraph::graph_from_adjacency_matrix(A, mode = "undirected", weighted = NULL)
 
   # Calculate estimate of the parameter from observed graph
   # which will remain same after generating a new graph on same fiber
@@ -59,5 +66,5 @@ goftest <- function(G_obs, C, numGraphs = 100) {
   # Output:
   # chi_seq: sequence of chi square test statistics on the sampled graphs
   # pvalue: estimated p-value when true model is ERSBM
-  return(list(chi_seq, pvalue))
+  return(list(statistic = chi_seq[1], p.value = pvalue))
 }
