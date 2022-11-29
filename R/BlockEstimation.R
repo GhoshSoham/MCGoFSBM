5a46a4573c9c0d45041d8bfb63afe28d1de881bd
Soham Ghosh <sohamghosh.isical@gmail.com>
2022-11-29 17:33:36 -0600

    attach stat package with kmeans in block_est function

--- R/BlockEstimation.R
+++ R/BlockEstimation.R
@@ -1,15 +1,12 @@
-#' Title
-#'
-#' @param A a n by n binary symmetric adjacency matrix representing a undirected graph where n is the no nodes in the graph
-#' @param K a numeric scalar representing no of blocks
-#'
-#' @return cluster: a vector of size n representing block assignment for each node; values are 1 to K i.e, no of cluster
-#'
-#' @examples
+# A a n by n binary symmetric adjacency matrix representing a undirected graph where n is the no nodes in the graph
+# K a numeric scalar representing no of blocks
+#
+# cluster: a vector of size n representing block assignment for each node; values are 1 to K i.e, no of cluster
+
 block_est <- function (A, K)
 {
   SVD <- svd(A, nu = K, nv = K)
 
-  km <- kmeans(SVD$v[, 1:K], centers = K, nstart = 30, iter.max = 30)
+  km <- stats::kmeans(SVD$v[, 1:K], centers = K, nstart = 30, iter.max = 30)
   return(cluster = km$cluster)
 }
