363679042c6fb5df48a4e38a2ea8251fc1305771
Ghosh <sohamghosh@tamu.edu>
2022-12-05 15:14:30 -0600

    add another function "get_edgelist_comp" which gives all the edges of the complement graph as output

--- src/EdgeList.cpp
+++ src/EdgeList.cpp
@@ -8,21 +8,49 @@ using namespace Rcpp;
 // n - no of nodes in the graph
 // [[Rcpp::export]]
 arma::mat get_edgelist(const arma::mat& adj, const int E, const int n){
-  // Initialize beta_mat and beta_start with all zeros
+  // Initialize edge_mat with all zeros
   arma::mat edge_mat(E, 2); edge_mat.zeros();
 
+  // If there is an edge between two nodes, it will add those two nodes in edge_mat
   int e = 0;
-  for (int row = 0; row<n; row++) {
-    for (int col = n-1; col>row; col--) {
+  for (int row = 0; row < n; row ++) {
+    for (int col = n - 1; col > row; col --) {
       if (adj(row, col) == 1){
         edge_mat(e, 0) = row + 1;
         edge_mat(e, 1) = col + 1;
-        e = e+1;
+        e = e + 1;
       }
     }
   }
 
   // Return output
   // edge_mat - a E x 2 matrix containing the edges of the graph
+  return edge_mat;
+}
+
+// Getting edge list of a complement graph in matrix
+// adj - adjacency matrix of a graph
+// E - no of edges in the complement graph
+// n - no of nodes in the graph
+// [[Rcpp::export]]
+arma::mat get_edgelist_comp(const arma::mat& adj, const int E, const int n){
+  // Initialize edge_mat with all zeros
+  arma::mat edge_mat(E, 2); edge_mat.zeros();
+
+  // If there is no edge between two nodes i.e., there is an edge in the complement graph
+  // it will add those two nodes in edge_mat
+  int e = 0;
+  for (int row = 0; row < n; row ++) {
+    for (int col = n - 1; col > row; col --) {
+      if (adj(row, col) == 0){
+        edge_mat(e, 0) = row + 1;
+        edge_mat(e, 1) = col + 1;
+        e = e + 1;
+      }
+    }
+  }
+
+  // Return output
+  // edge_mat - a E x 2 matrix containing the edges of the complement graph
   return edge_mat;
 }
