52eb5761090ea56deb8263aecf392495ac5ad8e2
GhoshSoham <sohamghosh.isical@gmail.com>
2022-12-09 01:00:42 -0600

    add the function graph_chain_on_fiber which samples several graphs on same fiber and give GoF test statistic as output; also change position of a function and add some comments

--- src/SampleMoveCpp.cpp
+++ src/SampleMoveCpp.cpp
@@ -30,6 +30,7 @@ arma::umat get_edgelist_cpp(const arma::mat& adj, const arma::vec& C, const int 
   return edge_mat;
 }
 
+
 // Getting edge list of a complement graph in matrix
 // adj - adjacency matrix of a graph
 // E - no of edges in the complement graph
@@ -59,38 +60,8 @@ arma::umat get_edgelist_comp_cpp(const arma::mat& adj, const arma::vec& C, const
   return edge_mat;
 }
 
-// A - adjacency matrix of a graph
-// p_mle - k*k matrix of MLE table
-// C - numeric vector of size n of block assignment; from 1 to k
-// n - no of nodes in the graph
-// k - no of blocks in the graph
-// [[Rcpp::export]]
-double graphchi_cpp(const arma::mat& A, const arma::mat& p_mle, const arma::vec& C, const int n, const int k){
-  // Initialize degree sequence and expected nodes matrix with all zeros
-  arma::mat degseq(n, k); degseq.zeros();
-  arma::mat exp_mat(n, k); exp_mat.zeros();
 
-  for(int inode=0; inode<n; inode++){
-    arma::vec indexRow = arma::vectorise(A.row(inode));
-    for(int iblock=0; iblock<k; iblock++){
-      arma::uvec indexCol= arma::find(C == iblock + 1);
-      // Observed no of neighbors of a node in each block
-      degseq(inode, iblock) = arma::accu(indexRow(indexCol));
-      int ni = indexCol.size(); // no of nodes in ith block
-      // Expected no of neighbors of a node in each block
-      exp_mat(inode, iblock) = p_mle(iblock, C(inode)-1) * ni;
-    }
-  }
-
-  // Calculate the value of the chi-sq statistic
-  double output = arma::accu(arma::pow(degseq.elem(arma::find(exp_mat != 0)) - exp_mat.elem(arma::find(exp_mat != 0)), 2) / exp_mat.elem(arma::find(exp_mat != 0)));
-
-  // Return output
-  // Output:
-  // the value of the chi-sq statistic
-  return output;
-}
-
+// Estimate the parameter of the model
 // A - adjacency matrix of a graph
 // C - numeric vector of size n of block assignment; from 1 to k
 // k - no of blocks in the graph
@@ -125,14 +96,52 @@ arma::mat get_mle_cpp(const arma::mat& A, const arma::vec& C, const int k){
   return arma::mat (table_obs / tot_edge);
 }
 
-// Getting edge list in matrix
+
+// Calculate the chi square test statistic using the estimated value of the parameters
+// A - adjacency matrix of a graph
+// p_mle - k*k matrix of MLE table
+// C - numeric vector of size n of block assignment; from 1 to k
+// n - no of nodes in the graph
+// k - no of blocks in the graph
+// [[Rcpp::export]]
+double graphchi_cpp(const arma::mat& A, const arma::mat& p_mle, const arma::vec& C, const int n, const int k){
+  // Initialize degree sequence and expected nodes matrix with all zeros
+  arma::mat degseq(n, k); degseq.zeros();
+  arma::mat exp_mat(n, k); exp_mat.zeros();
+
+  for(int inode=0; inode<n; inode++){
+    arma::vec indexRow = arma::vectorise(A.row(inode));
+    for(int iblock=0; iblock<k; iblock++){
+      arma::uvec indexCol= arma::find(C == iblock + 1);
+      // Observed no of neighbors of a node in each block
+      degseq(inode, iblock) = arma::accu(indexRow(indexCol));
+      int ni = indexCol.size(); // no of nodes in ith block
+      // Expected no of neighbors of a node in each block
+      exp_mat(inode, iblock) = p_mle(iblock, C(inode)-1) * ni;
+    }
+  }
+
+  // Calculate the value of the chi-sq statistic
+  double output = arma::accu(arma::pow(degseq.elem(arma::find(exp_mat != 0)) - exp_mat.elem(arma::find(exp_mat != 0)), 2) / exp_mat.elem(arma::find(exp_mat != 0)));
+
+  // Return output
+  // Output:
+  // the value of the chi-sq statistic
+  return output;
+}
+
+
+// Sample a graph in the same fiber
+// The true model is ERSBM
 // adj - adjacency matrix of a graph
 // C - numeric vector of size n of block assignment; from 1 to k
 // n - no of nodes in the graph
 // k - no of blocks in the graph
 // [[Rcpp::export]]
 arma::mat sample_a_move_cpp(const arma::mat& adj, const arma::vec& C, const int n, const int k){
+  // Assigning adj to a new variable for ease of use
   arma::mat A = adj;
+
   // Getting edge information of the graph
   int num_edges = arma::accu(A) / 2;
   arma::umat all_edges = get_edgelist_cpp(A, C, num_edges, n);
@@ -235,3 +244,37 @@ arma::mat sample_a_move_cpp(const arma::mat& adj, const arma::vec& C, const int 
   return A;
 }
 
+
+// Computing chi-square GoF statistic several times for each of the sampled graph on
+// same fiber after estimating the parameter
+// A - adjacency matrix of a graph
+// C - numeric vector of size n of block assignment; from 1 to k
+// n - no of nodes in the graph
+// k - no of blocks in the graph
+// numGraphs - no of times chain will move to produce a new graph
+// [[Rcpp::export]]
+arma::vec graph_chain_on_fiber(const arma::mat& A, const arma::vec& C, const int n, const int k, const int numGraphs){
+  // Defining new variables to store the test statistics value
+  arma::vec chi_seq_cpp(numGraphs); chi_seq_cpp.zeros();
+  arma::mat A_new = A;
+  arma::mat A_current = A_new;
+
+  // Estimating the parameters from observed graph
+  // which will remain same after generating a new graph on same fiber
+  arma::mat p_mle = get_mle_cpp(A, C, k);
+
+  // Storing the GoF statistic on observed graph
+  chi_seq_cpp(0) = graphchi_cpp(A, p_mle, C, n, k);
+  for(int iter = 1; iter<numGraphs; iter++){
+    // Sampling a new graph on same fiber
+    A_current = sample_a_move_cpp(A_new, C, n, k);
+
+    // Computing chi-square GoF test statistic on new sampled graph
+    chi_seq_cpp(iter) = graphchi_cpp(A_current, p_mle, C, n, k);
+    A_new = A_current;
+  }
+
+  // Output
+  // chi_seq_cpp - a vector containing value of the statistic of each sample
+  return chi_seq_cpp;
+}
