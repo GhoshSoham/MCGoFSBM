3f501c229c29dfbfad3a70e85cb29aec8f9be4c5
GhoshSoham <sohamghosh.isical@gmail.com>
2022-12-08 23:10:39 -0600

    copy two other functions graphchi_cpp and get_mle_cpp also in the SampleMoveCpp script to create a new function using these functions

--- src/SampleMoveCpp.cpp
+++ src/SampleMoveCpp.cpp
@@ -59,3 +59,68 @@ arma::umat get_edgelist_comp_cpp(const arma::mat& adj, const arma::vec& C, const
   return edge_mat;
 }
 
+// A - adjacency matrix of a graph
+// p_mle - k*k matrix of MLE table
+// C - numeric vector of size n of block assignment; from 1 to k
+// n - no of nodes in the graph
+// k - no of blocks in the graph
+// [[Rcpp::export]]
+double graphchi_cpp(const arma::mat& A, const arma::mat& p_mle, const arma::vec& C, const int n, const int k){
+  // Initialize degree sequence and expected nodes matrix with all zeros
+  arma::mat degseq(n, k); degseq.zeros();
+  arma::mat exp_mat(n, k); exp_mat.zeros();
+
+  for(int inode=0; inode<n; inode++){
+    arma::vec indexRow = arma::vectorise(A.row(inode));
+    for(int iblock=0; iblock<k; iblock++){
+      arma::uvec indexCol= arma::find(C == iblock + 1);
+      // Observed no of neighbors of a node in each block
+      degseq(inode, iblock) = arma::accu(indexRow(indexCol));
+      int ni = indexCol.size(); // no of nodes in ith block
+      // Expected no of neighbors of a node in each block
+      exp_mat(inode, iblock) = p_mle(iblock, C(inode)-1) * ni;
+    }
+  }
+
+  // Calculate the value of the chi-sq statistic
+  double output = arma::accu(arma::pow(degseq.elem(arma::find(exp_mat != 0)) - exp_mat.elem(arma::find(exp_mat != 0)), 2) / exp_mat.elem(arma::find(exp_mat != 0)));
+
+  // Return output
+  // Output:
+  // the value of the chi-sq statistic
+  return output;
+}
+
+// A - adjacency matrix of a graph
+// C - numeric vector of size n of block assignment; from 1 to k
+// k - no of blocks in the graph
+// [[Rcpp::export]]
+arma::mat get_mle_cpp(const arma::mat& A, const arma::vec& C, const int k){
+
+  // Initialize block_card, a vector of length of k with all zeros
+  arma::colvec block_card(k); block_card.zeros();
+
+
+  // Initialize table_obs, a k x k matrix with all zeros
+  arma::mat table_obs(k, k); table_obs.zeros();
+
+  for (int row = 0; row < k; row ++) {
+    arma::uvec indexRow = arma::find(C == row + 1);
+    // block_card will contain no of elements in each block
+    block_card(row) = indexRow.size();
+    for (int col = 0; col < k; col ++) {
+      arma::uvec indexCol = arma::find(C == col + 1);
+      // table_obs will contain total observed edges between block and within block
+      table_obs(row, col) = arma::accu(A(indexRow, indexCol));
+    }
+  }
+
+  // Calculate total possible edges between and within block based on nodes information
+  arma::mat tot_edge = block_card * block_card.t() - arma::diagmat(block_card);
+
+
+  // Return output
+  // Output:
+  // the estimated q_i,j matrix (the probability of edges between block i and j)
+  return arma::mat (table_obs / tot_edge);
+}
